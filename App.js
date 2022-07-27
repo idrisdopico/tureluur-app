c13c0a252a0203ffcdc90de07e98392b74c019a8
Idris Dopico Peña <dopico1996@hotmail.com>
2022-07-27 21:26:09 +0200

    Add theme colors

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from "./app/theme/colors";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: colors.backgroundColor }]}>
       <Text>Plaats hier je bestelling</Text>
       <Text>Bestellingen geplaatst na 20:00 worden de volgende dag niet meer mee genomen</Text>
     </View>
---
+++ app/theme/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  backgroundColor: '#d7ded8',
+  primaryColor: '#3b5a3d',
+  secondaryColor: '#f8f7f8'
+}
